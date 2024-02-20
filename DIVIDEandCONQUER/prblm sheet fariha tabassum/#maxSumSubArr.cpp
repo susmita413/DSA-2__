ef256ff97e78ab3337daac186b1b0c1d6867db47
Susmita Paul <137711194+susmita413@users.noreply.github.com>
2024-02-20 06:06:08 +0600

    Update #maxSumSubArr.cpp

--- DIVIDEandCONQUER/prblm sheet fariha tabassum/#maxSumSubArr.cpp
+++ DIVIDEandCONQUER/prblm sheet fariha tabassum/#maxSumSubArr.cpp
@@ -4,33 +4,34 @@
 using namespace std ;
 
 
-int maximum(int a, int b, int c){
+int maximum(int a, int b, int c){  // Function to return the maximum of three numbers
     if(a>=b && a>=c) return a;
     else if(b>=a && b>=c) return b;
     else return c;
 }
 
-int maxCrossing_SubArray(int a[], int low, int high,int mid){
-    int left_res = INT_MIN;
+int maxCrossing_SubArray(int a[], int low, int high,int mid){ //find the maximum possible sum of a subarray
+                                                              // crossing the midpoint
+    int left_res = INT_MIN;      // Initialize left_res to the smallest possible integer
     int sum=0 ;
-    for(int i=mid; i>=low; i--){
+    for(int i=mid; i>=low; i--){   // Calculate maximum sum of subarray ending at midpoint on the left side
        sum= sum+ a[i];
-        if(left_res<= sum){
-            left_res= sum ;
+        if(left_res<= sum){      // here sum= sum of all leftSide ELEMENTS
+            left_res= sum ;      // left_res= only maximum sum of left subarray
         }
     }
-    int right_res = INT_MIN;
+    int right_res = INT_MIN;     // Initialize right_res to the smallest possible integer
     sum=0 ;
-    for(int i=mid+1; i<=high; i++){
+    for(int i=mid+1; i<=high; i++){   // Calculate maximum sum of subarray starting from midpoint on the right side
         sum= sum + a[i];
-        if(right_res <= sum){
-            right_res= sum ;
+        if(right_res <= sum){     // here sum= sum of all RIGHT Side ELEMENTS
+            right_res= sum ;      // right_res= only maximum sum of RIGHT subarray
         }
     }
-    return left_res+right_res ;
+    return left_res+right_res ;  // Return MAXIMUM SUM on left and right of midpoint
 }
 
-int max_sumSubArray(int a[], int low, int high){
+int max_sumSubArray(int a[], int low, int high){  // Main function implementing the Divide and Conquer approach
     if(low==high){
         return a[low];
     }
